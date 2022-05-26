364e3ea85d8f08b1b32370d55f3f3d1ece945c8c
chetan.vashistth_india.nec.com <chetan.vashistth@india.nec.com>
2022-05-26 13:49:01 +0530

    first program solution

---
+++ solutions/First.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class First{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        while (number > 0){
+            System.out.println(number % 10);
+            number = number/10;
+        }
+        
+    }
+}
