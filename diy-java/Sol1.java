60a467a410a28c09a05653b1ae19b48d780322f8
mysukiru <mysukiru@gmail.com>
2022-05-26 16:27:20 +0530

    Hint added for Problem 1

---
+++ diy-java/Sol1.java
@@ -0,0 +1,13 @@
+// Import Java Scanner to accept input
+
+class Sol1{
+    public static void main(String[] args) {
+        // Create new Scanner instance
+        // Read Integer from command line
+        // put a while loop
+            // Get one's place digit by modulo 10
+            // Print one's place digit
+            // Remove the one's place digit by dividing by 10
+        }
+        
+    }
