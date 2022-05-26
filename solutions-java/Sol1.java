7615de7a8695f40f3ce36bcc413195132f4e47bd
mysukiru <mysukiru@gmail.com>
2022-05-26 16:21:51 +0530

    Comments inserted

--- solutions-java/Sol1.java
+++ solutions-java/Sol1.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.util.Scanner; // Scanner is a standard Java Library to recieve input from command line
 class Sol1{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        while (number > 0){
-            System.out.println(number % 10);
-            number = number/10;
+        Scanner sc = new Scanner(System.in); // Create an instance of Scanner
+        int number = sc.nextInt(); // nextInt is to read Integer type input
+        while (number > 0){ 
+            System.out.println(number % 10); // % operator returns remainder
+            number = number/10; // / operator returns quotient
         }
         
     }
