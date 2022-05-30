946ddaac13b30cb23ad2d6125707b5b22bced432
mysukiru <mysukiru@gmail.com>
2022-05-30 21:42:48 +0530

    Solution 5

---
+++ solutions-java/Sol5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Sol5{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    }
+}
