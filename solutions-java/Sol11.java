a7d5179d248f1506db29eff14c423a26d5c06712
mysukiru <mysukiru@gmail.com>
2022-06-01 21:50:08 +0530

    clean up

--- solutions-java/Sol11.java
+++ solutions-java/Sol11.java
@@ -2,10 +2,6 @@ import java.lang.*;
 import java.util.*;
 public class Sol11 {
         public static void main(String[] args) {
-        // YOUR CODE GOES HERE
-        // Please take input and print output to standard input/output (stdin/stdout)
-        // DO NOT USE ARGUMENTS FOR INPUTS
-        // E.g. 'Scanner' for input & 'System.out' for output
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         for (int i = 1; i <= N; i++){
